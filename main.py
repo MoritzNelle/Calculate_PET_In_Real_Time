03f35424f59426df0549e6b8e6c25ca2c10cdff9
Moritz <152723862+CropCoder@users.noreply.github.com>
2024-05-23 08:53:09 +0200

    Refactor variable names and improve formatting.

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_save_data(url):
 
     # Create a file name with the timestamp
     global filename
-    filename = f"entrie_raw_data_24h/scraped_text_{timestamp}.csv"
+    filename = f"entrie_raw_data_24h/raw_data_{timestamp}.csv"
 
     with open(filename, "w") as file:    # Save the scraped text into a file
         file.write(response.text)
@@ -92,33 +92,33 @@ def extract_data(filename):
 
 def calc_PET():
     T_mean  = weather_data_avg.temperature      # Mean temperature in degrees Celsius
-    Rs      = weather_data_avg.solar_radiation  # Solar radiation in MJ/m²/day
-    u2      = weather_data_avg.wind_speed       # Wind speed at 2 meters in m/s
+    R_s      = weather_data_avg.solar_radiation  # Solar radiation in MJ/m²/day
+    u_2      = weather_data_avg.wind_speed       # Wind speed at 2 meters in m/s
     DELTA   = 4098 * (0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3))) / ((T_mean + 237.3) ** 2) # Slope of the saturation vapor pressure-temperature curve in kPa/°C
     p       = weather_data_avg.pressure         # Atmospheric pressure in kPa
     y       = (1.013**-3 * p) / 0.622           # Psychrometric constant in kPa/°C
-    DT      = DELTA / (DELTA + y * (1 + 0.34 * u2)) # Slope of the vapor pressure-temperature curve in kPa/°C
-    PT      = y/(DELTA+y*(1+0.34*u2))           # Psychrometric constant in kPa/°C
-    TT      = 900/(T_mean + 273.3)*u2           # Temperature term in kPa/°C
-    es      = 0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3)) # Saturation vapor pressure in kPa
-    ea      = es * (weather_data_avg.humidity/100)  # Actual vapor pressure in kPa
+    DT      = DELTA / (DELTA + y * (1 + 0.34 * u_2)) # Slope of the vapor pressure-temperature curve in kPa/°C
+    PT      = y/(DELTA+y*(1+0.34*u_2))           # Psychrometric constant in kPa/°C
+    TT      = 900/(T_mean + 273.3)*u_2           # Temperature term in kPa/°C
+    e_s      = 0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3)) # Saturation vapor pressure in kPa
+    e_a      = e_s * (weather_data_avg.humidity/100)  # Actual vapor pressure in kPa
     dr      = 1 + 0.033 * math.cos((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday) # Inverse relative distance Earth-Sun
     delta   = 0.409 * math.sin((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday - 1.39) # Solar declination in radians
     lat_rad = math.radians(51.989)              #phi
-    ws      = math.acos(-math.tan(lat_rad) * math.tan(delta)) # Sunset hour angle in radians
-    Ra      = 24 * 60 / math.pi * 0.082 * dr * (ws * math.sin(lat_rad) * math.sin(delta) + math.cos(lat_rad) * math.cos(delta) * math.sin(ws)) # Extraterrestrial radiation in MJ/m²/day
-    Rso     = (0.75 + 2 * 10**-5 * 9.5) * Ra    # Clear-sky solar radiation in MJ/m²/day
-    Rns     = (1 - 0.23) * Rs                   # Net solar radiation in MJ/m²/day
-    Rnl     = (T_mean+273.16)**4 * (0.34-0.14*math.sqrt(ea)) * (1.35 * Rs / Rso - 0.35) # Net outgoing longwave radiation in MJ/m²/day
-    Rn      = Rns - Rnl                         # Net radiation in MJ/m²/day
-    R_ng    = Rn * 0.408                        # Net radiation in MJ/m²/day
+    w_s      = math.acos(-math.tan(lat_rad) * math.tan(delta)) # Sunset hour angle in radians
+    R_a      = 24 * 60 / math.pi * 0.082 * dr * (w_s * math.sin(lat_rad) * math.sin(delta) + math.cos(lat_rad) * math.cos(delta) * math.sin(w_s)) # Extraterrestrial radiation in MJ/m²/day
+    R_so     = (0.75 + 2 * 10**-5 * 9.5) * R_a    # Clear-sky solar radiation in MJ/m²/day
+    R_ns     = (1 - 0.23) * R_s                   # Net solar radiation in MJ/m²/day
+    R_nl     = 4.903e-9 * (T_mean+273.16)**4 * (0.34-0.14*math.sqrt(e_a)) * (1.35 * R_s / R_so - 0.35) 
+    R_n      = R_ns - R_nl                         # Net radiation in MJ/m²/day
+    R_ng    = R_n * 0.408                        # Net radiation in MJ/m²/day
     ET_rad  = DT * R_ng                         # Radiation term in mm/day
-    ET_wind = PT * TT * (es - ea)               # Wind term in mm/day
+    ET_wind = PT * TT * (e_s - e_a)               # Wind term in mm/day
     
     global ET_0
     ET_0    = ET_rad + ET_wind                  # Potential evapotranspiration in mm/day
 
-    #print("T_mean:\t", T_mean, "\nRs:\t", Rs, "\nu2:\t", u2, "\nDELTA:\t", DELTA, "\np:\t", p, "\ny:\t", y, "\nDT:\t", DT, "\nPT:\t", PT, "\nTT:\t", TT, "\nes:\t", es, "\nea:\t", ea, "\ndr:\t", dr, "\ndelta:\t", delta, "\nlat_rad:", lat_rad, "\nws:\t", ws, "\nRa:\t", Ra, "\nRso:\t", Rso, "\nRns:\t", Rns, "\nRnl:\t", Rnl, "\nRn:\t", Rn, "\nR_ng:\t", R_ng, "\nET_rad:\t", ET_rad, "\nET_wind:", ET_wind, "\nET_0:\t", ET_0)
+    print("T_mean:\t", round(T_mean, 3), "C\nRs:\t", round(R_s, 3), "MJ/m²/day\nu2:\t", round(u_2, 3), "m/s\nDELTA:\t", round(DELTA, 3), "kPa/°C\np:\t", round(p, 3), "kPa\ny:\t", round(y, 3), "kPa/°C\nDT:\t", round(DT, 3), "kPa/°C\nPT:\t", round(PT, 3), "kPa/°C\nTT:\t", round(TT, 3), "kPa/°C\nes:\t", round(e_s, 3), "kPa\nea:\t", round(e_a, 3), "kPa\ndr:\t", round(dr, 3), "\ndelta:\t", round(delta, 3), "\nlat_rad:", round(lat_rad, 3), "\nws:\t", round(w_s, 3), "\nRa:\t", round(R_a, 3), "MJ/m²/day\nRso:\t", round(R_so, 3), "MJ/m²/day\nRns:\t", round(R_ns, 3), "MJ/m²/day\nRnl:\t", round(R_nl, 3), "MJ/m²/day\nRn:\t", round(R_n, 3), "MJ/m²/day\nR_ng:\t", round(R_ng, 3), "MJ/m²/day\nET_rad:\t", round(ET_rad, 3), "mm/day\nET_wind:", round(ET_wind, 3), "mm/day\nET_0:\t", round(ET_0, 3), "mm/day")
 
     return ET_0
 
