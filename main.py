5d606bd86c8d14047e38d967fde27b384a781bfc
Moritz <152723862+CropCoder@users.noreply.github.com>
2024-05-27 07:50:26 +0200

    Optimizing code by combineing functions: calc_PET_24h() and calc_PET_since_sowing() -> calc_PET() calc_water_deficit_24h() and calc_water_deficit_since_sowing() -> calc_water_deficit()

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def get_save_data(url):#MARK: get and save data
     global filename    # Create a file name with the timestamp
 
     if isinstance(url, str): #only one URL, not a list of URLs, 24h data
-        print ("URL is a string")
+        #print ("URL is a string")
 
         response = requests.get(url)    # Send a GET request to the website
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")    # Get the current timestamp
@@ -177,51 +177,18 @@ def extract_data(filename): #MARK: Extract data from file
         raise ValueError("Filename must be a string or a list of strings")
 
 
-def calc_PET_24():
-    T_mean   = weather_data_avg_24h.temperature      # Mean temperature in degrees Celsius
-    R_s      = weather_data_avg_24h.solar_radiation  # Solar radiation in MJ/m²/day
-    u_2      = weather_data_avg_24h.wind_speed       # Wind speed at 2 meters in m/s
+def calc_PET(weather_data):
+    T_mean   = weather_data.temperature      # Mean temperature in degrees Celsius
+    R_s      = weather_data.solar_radiation  # Solar radiation in MJ/m²/day
+    u_2      = weather_data.wind_speed       # Wind speed at 2 meters in m/s
     DELTA    = 4098 * (0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3))) / ((T_mean + 237.3) ** 2) # Slope of the saturation vapor pressure-temperature curve in kPa/°C
-    p        = weather_data_avg_24h.pressure         # Atmospheric pressure in kPa
+    p        = weather_data.pressure         # Atmospheric pressure in kPa
     y        = (1.013**-3 * p) / 0.622               # Psychrometric constant in kPa/°C
     DT       = DELTA / (DELTA + y * (1 + 0.34 * u_2))# Slope of the vapor pressure-temperature curve in kPa/°C
     PT       = y/(DELTA+y*(1+0.34*u_2))              # Psychrometric constant in kPa/°C
     TT       = 900/(T_mean + 273.3)*u_2              # Temperature term in kPa/°C
     e_s      = 0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3)) # Saturation vapor pressure in kPa
-    e_a      = e_s * (weather_data_avg_24h.humidity/100)  # Actual vapor pressure in kPa
-    dr       = 1 + 0.033 * math.cos((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday) # Inverse relative distance Earth-Sun
-    delta    = 0.409 * math.sin((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday - 1.39) # Solar declination in radians
-    lat_rad  = math.radians(51.989)                  #phi
-    w_s      = math.acos(-math.tan(lat_rad) * math.tan(delta)) # Sunset hour angle in radians
-    R_a      = 24 * 60 / math.pi * 0.082 * dr * (w_s * math.sin(lat_rad) * math.sin(delta) + math.cos(lat_rad) * math.cos(delta) * math.sin(w_s)) # Extraterrestrial radiation in MJ/m²/day
-    R_so     = (0.75 + 2 * 10**-5 * 9.5) * R_a       # Clear-sky solar radiation in MJ/m²/day
-    R_ns     = (1 - 0.23) * R_s                      # Net solar radiation in MJ/m²/day
-    R_nl     = 4.903e-9 * (T_mean+273.16)**4 * (0.34-0.14*math.sqrt(e_a)) * (1.35 * R_s / R_so - 0.35) 
-    R_n      = R_ns - R_nl                           # Net radiation in MJ/m²/day
-    R_ng     = R_n * 0.408                           # Net radiation in MJ/m²/day
-    ET_rad   = DT * R_ng                             # Radiation term in mm/day
-    ET_wind  = PT * TT * (e_s - e_a)                 # Wind term in mm/day
-    
-    global ET_0
-    ET_0    = ET_rad + ET_wind                  # Potential evapotranspiration in mm/day
-
-    #print("T_mean:\t", round(T_mean, 3), "C\nRs:\t", round(R_s, 3), "MJ/m²/day\nu2:\t", round(u_2, 3), "m/s\nDELTA:\t", round(DELTA, 3), "kPa/°C\np:\t", round(p, 3), "kPa\ny:\t", round(y, 3), "kPa/°C\nDT:\t", round(DT, 3), "kPa/°C\nPT:\t", round(PT, 3), "kPa/°C\nTT:\t", round(TT, 3), "kPa/°C\nes:\t", round(e_s, 3), "kPa\nea:\t", round(e_a, 3), "kPa\ndr:\t", round(dr, 3), "\ndelta:\t", round(delta, 3), "\nlat_rad:", round(lat_rad, 3), "\nws:\t", round(w_s, 3), "\nRa:\t", round(R_a, 3), "MJ/m²/day\nRso:\t", round(R_so, 3), "MJ/m²/day\nRns:\t", round(R_ns, 3), "MJ/m²/day\nRnl:\t", round(R_nl, 3), "MJ/m²/day\nRn:\t", round(R_n, 3), "MJ/m²/day\nR_ng:\t", round(R_ng, 3), "MJ/m²/day\nET_rad:\t", round(ET_rad, 3), "mm/day\nET_wind:", round(ET_wind, 3), "mm/day\nET_0:\t", round(ET_0, 3), "mm/day")
-
-    return ET_0
-
-
-def calc_PET_sowing():
-    T_mean   = weather_data_avg_sowing.temperature      # Mean temperature in degrees Celsius
-    R_s      = weather_data_avg_sowing.solar_radiation  # Solar radiation in MJ/m²/day
-    u_2      = weather_data_avg_sowing.wind_speed       # Wind speed at 2 meters in m/s
-    DELTA    = 4098 * (0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3))) / ((T_mean + 237.3) ** 2) # Slope of the saturation vapor pressure-temperature curve in kPa/°C
-    p        = weather_data_avg_sowing.pressure         # Atmospheric pressure in kPa
-    y        = (1.013**-3 * p) / 0.622               # Psychrometric constant in kPa/°C
-    DT       = DELTA / (DELTA + y * (1 + 0.34 * u_2))# Slope of the vapor pressure-temperature curve in kPa/°C
-    PT       = y/(DELTA+y*(1+0.34*u_2))              # Psychrometric constant in kPa/°C
-    TT       = 900/(T_mean + 273.3)*u_2              # Temperature term in kPa/°C
-    e_s      = 0.6108 * math.exp((17.27 * T_mean) / (T_mean + 237.3)) # Saturation vapor pressure in kPa
-    e_a      = e_s * (weather_data_avg_sowing.humidity/100)  # Actual vapor pressure in kPa
+    e_a      = e_s * (weather_data.humidity/100)  # Actual vapor pressure in kPa
     dr       = 1 + 0.033 * math.cos((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday) # Inverse relative distance Earth-Sun
     delta    = 0.409 * math.sin((2 * math.pi) / 365 * datetime.now().timetuple().tm_yday - 1.39) # Solar declination in radians
     lat_rad  = math.radians(51.989)                  #phi
@@ -314,15 +281,10 @@ def visulize_data():
 
     plt.show()
 
-def calc_water_deficit_24():
+def calc_water_deficit(weather_data):
     water_content = 0
-    water_deficit = water_content + (ET_0 - weather_data_avg_24h.rain)
+    water_deficit = water_content + (ET_0 - weather_data.rain)
     return water_deficit
-
-def calc_water_deficit_sowing():
-    water_content = 0
-    water_deficit = water_content + (ET_0 - weather_data_avg_sowing.rain)
-    return water_deficit    
 
 
 
@@ -332,8 +294,8 @@ if __name__ == "__main__":
     extract_data(filename)
 
     print ("\n24H REPORT:")
-    print ("Potential evapotranspiration:\t",calc_PET_24(), "mm/day")
-    print ("Water deficit:\t\t\t", calc_water_deficit_24(), "mm/day\n")
+    print ("Potential evapotranspiration:\t",calc_PET(weather_data_avg_24h), "mm/day")
+    print ("Water deficit:\t\t\t", calc_water_deficit(weather_data_avg_24h), "mm/day\n")
     #print (weather_data_avg_24h.temperature, weather_data_avg_24h.humidity, weather_data_avg_24h.wind_speed, weather_data_avg_24h.solar_radiation, weather_data_avg_24h.pressure, weather_data_avg_24h.rain)
 
 
@@ -341,8 +303,8 @@ if __name__ == "__main__":
     extract_data(file_names())
     
     print ("SINCE SOWING REPORT (untill last midnight):")
-    print ("Acumulative potential evapotranspiration:\t",calc_PET_sowing(), "mm/day")
-    print ("Acumulative water deficit:\t\t\t", calc_water_deficit_sowing(), "mm/day\n")
+    print ("Acumulative potential evapotranspiration:\t",calc_PET(weather_data_avg_sowing), "mm/day")
+    print ("Acumulative water deficit:\t\t\t", calc_water_deficit(weather_data_avg_sowing), "mm/day\n")
 
 
     #visulize_data()
